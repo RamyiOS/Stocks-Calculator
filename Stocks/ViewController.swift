cd29fb8c304f73acaedddcf72e92b92addd8b9e6
Ramy <Ramy_naser@icloud.com>
2022-06-25 04:15:29 +0200

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Mac on 6/25/22.
+//  Copyright © 2022 ramy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
